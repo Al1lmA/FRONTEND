b16a8dce47f8a74183986490cda052e713400005
Yana Zhdanova <zhdanova.yana2003@yandex.ru>
2023-10-30 15:35:07 +0300

    for pages

--- bankservices/src/components/main.tsx
+++ bankservices/src/components/main.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 root.render(
     <BrowserRouter>
             <Routes>
-                <Route path="services/" element={<Services/>}/>
+                <Route path="FRONTENT/" element={<Services/>}/>
                 <Route path="services/:id" element={<Service/>} />
             </Routes>
     </BrowserRouter>
